43b84767fe08b397a68e046335de661cab1fbfc1
Frenzy017 <aleksladjov4@gmail.com>
2025-10-04 18:32:25 +0300

    Update logic

--- index.js
+++ index.js
@@ -5,3 +5,49 @@
 */
 
 // Basic conversion factors (from -> to)
+
+const digit = document.getElementById("digit").textContent;
+const convertButton = document.getElementById("convert-button");
+const lengthText = document.getElementById("length");
+const volumeText = document.getElementById("volume");
+const massText = document.getElementById("mass");
+
+function lengthConversion() {
+  const meterToFeetConversion = digit * 3.281;
+  const feetToMeterConversion = digit / 3.281;
+
+  convertButton.addEventListener("click", () => {
+    lengthText.textContent = `
+         ${digit} meters = ${meterToFeetConversion.toFixed(3)} feet | 
+         ${digit} feet = ${feetToMeterConversion.toFixed(3)} meters
+         `;
+  });
+}
+
+function volumeConversion() {
+  const literToGallonConversion = digit / 3.785;
+  const gallonToLiterConversion = digit * 3.785;
+
+  convertButton.addEventListener("click", () => {
+    volumeText.textContent = `
+         ${digit} liters = ${literToGallonConversion.toFixed(3)} gallons | 
+         ${digit} gallons = ${gallonToLiterConversion.toFixed(3)} liters
+         `;
+  })
+}
+
+function massConversion() {
+  const kiloToPoundConversion = digit * 2.205;
+  const poundToKiloConversion = digit / 2.205;
+  
+  convertButton.addEventListener("click", () => {
+    massText.textContent = `
+         ${digit} kilos = ${kiloToPoundConversion.toFixed(3)} pounds | 
+         ${digit} pounds = ${poundToKiloConversion.toFixed(3)} kilos
+         `;
+  })
+}
+
+lengthConversion();
+volumeConversion();
+massConversion();
